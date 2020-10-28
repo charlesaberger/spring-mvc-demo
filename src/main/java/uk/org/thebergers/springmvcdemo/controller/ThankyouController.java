eb231766168a42e3a751ff8f2a5139d83991c62a
Charles Berger <charles@thebergers.org.uk>
2020-10-28 21:13:31 +0000

    Implement Post Redirect Get mechanism to carry forward submitted data between redirects. (#1)

--- src/main/java/uk/org/thebergers/springmvcdemo/controller/ThankyouController.java
+++ src/main/java/uk/org/thebergers/springmvcdemo/controller/ThankyouController.java
@@ -3,27 +3,17 @@ package uk.org.thebergers.springmvcdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import uk.org.thebergers.springmvcdemo.model.PageTwoBean;
 
 @Controller
-public class PageTwoController {
+public class ThankyouController {
 
-	@GetMapping(path = "/pagetwo")
+	@GetMapping(path = "/thankyou")
 	public ModelAndView display(@ModelAttribute("pageTwoBean")PageTwoBean pageTwoBean) {
 		ModelAndView result = new ModelAndView();
 		result.addObject("pageTwoBean", pageTwoBean);
-		result.setViewName("/pagetwo");
-		return result;
-	}
-
-	@PostMapping(path = "/pagetwo")
-	public ModelAndView submit(PageTwoBean pageTwoBean, final RedirectAttributes redirectAttributes) {
-		ModelAndView result = new ModelAndView();
-		redirectAttributes.addFlashAttribute("pageTwoBean", pageTwoBean);
-		result.setViewName("redirect:/thankyou");
+		result.setViewName("thankyou");
 		return result;
 	}
 }
